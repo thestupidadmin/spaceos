0578aea6b7cfe7454dce1b55264bc0a3c86695ce
thestupidadmin <admin@is-a.space>
2024-10-03 12:47:52 +0300

    Delete vcurl.py

--- vcurl.py
+++
@@ -1,6 +0,0 @@
-import requests
-url = input("Send GET request to :")
-if url.startswith(('http://', 'https://')):
-    print("Please retry the command without entering the protocol (http://, https://)")
-else:
-  requests.get(url)
