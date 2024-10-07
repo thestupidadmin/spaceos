3103108984bd3a9f2e3a45b2c86a7de8d4f1a805
thestupidadmin <admin@is-a.space>
2024-10-07 10:55:21 +0300

    Update get-file.py

--- repos/get-file.py
+++ repos/get-file.py
@@ -1,13 +1,40 @@
 import sys
 import subprocess
+import os
+import shutil
 
 def download(url):
     try:
         subprocess.check_call(["wget", url])
     except subprocess.CalledProcessError as e:
         print(f"Failed to download file from {url}: {e}")
+        return None
     except Exception as e:
         print(f"An error occurred: {e}")
+        return None
+    
+    filename = url.split("/")[-1]
+    return filename
+
+def move_file_to_bin(filename):
+    if not filename:
+        return
+    
+    current_dir = os.getcwd()
+    bin_dir = os.path.join(current_dir, 'bin')
+
+    if not os.path.exists(bin_dir):
+        print(f"Something went wrong.")
+        return
+    
+    src = os.path.join(current_dir, filename)
+    dest = os.path.join(bin_dir, filename)
+    
+    try:
+        shutil.move(src, dest)
+        print(f"Downloaded {filename}")
+    except Exception as e:
+        print(f"Failed to download {filename}: {e}")
 
 def main():
     if len(sys.argv) != 2:
@@ -15,7 +42,10 @@ def main():
         return
 
     url = sys.argv[1]
-    download(url)
+    filename = download(url)
+
+    if filename:
+        move_file_to_bin(filename)
 
 if __name__ == "__main__":
     main()
